ee4ea210821d8e35d667f7e5b1cc0b856f008aaf
Harrison Keating <harrisonrkeating@gmail.com>
2022-09-26 18:25:23 -0400

    Consolidates functionality of create.php into new.php

--- new.php
+++ new.php
@@ -1,12 +1,24 @@
 <?php
 require "loginCheck.php";
+require "connection.php";
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  if ($_POST['name'] and $_POST['body']) {
+    $sql = "INSERT INTO notes (owner, name, body) VALUES ('" . $_SESSION['user'] . "', '" . $_POST['name']  . "', '" . $_POST['body'] . "')";
+    $result = $conn->query($sql);
+    header("Location: /index.php");
+  }
+  else {
+    echo "Bad request";
+  }
+}
 ?>
 
 <html>
 <head>
 </head>
 <body>
-  <form action="create.php" method="POST">
+  <form action="new.php" method="POST">
     Name: <input type="text" name="name"><br>
     <textarea name="body" rows="5" cols="40"></textarea>
     <button type="submit">Create Note</button>
