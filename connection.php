1093eb0e7c71c6bc0f56f734388e82dceeeff253
Harrison Keating <harrisonrkeating@gmail.com>
2023-01-16 22:48:44 -0500

    Moves database credentials to environment variables instead of local file

--- connection.php
+++ connection.php
@@ -1,11 +1,28 @@
 <?php
-$vars = explode("\n", file_get_contents("connection.txt"));
-$servername = trim($vars[0]);
-$username = trim($vars[1]);
-$password = trim($vars[2]);
-$dbname = trim($vars[3]);
+$env = getenv();
+if (!array_key_exists('EASYNOTE_DB_IP', $env)) {
+	echo "<p>Please set the EASYNOTE_DB_IP environment variable.</p>";
+}
+else { $servername = $env["EASYNOTE_DB_IP"]; }
+
+
+if (!array_key_exists('EASYNOTE_DB_USER', $env)) {
+	echo "<p>Please set the EASYNOTE_DB_USER environment variable.</p>";
+}
+else { $username = $env["EASYNOTE_DB_USER"]; }
+
+if (!array_key_exists('EASYNOTE_DB_PASS', $env)) {
+	echo "<p>Please set the EASYNOTE_DB_PASS environment variable.</p>";
+}
+else { $password = $env["EASYNOTE_DB_PASS"]; }
+
+if (!array_key_exists('EASYNOTE_DB_NAME', $env)) {
+	echo "<p>Please set the EASYNOTE_DB_NAME environment variable.</p>";
+}
+else { $dbname = $env["EASYNOTE_DB_NAME"]; }
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+	die("Connection failed: " . $conn->connect_error);
 }
 ?>
